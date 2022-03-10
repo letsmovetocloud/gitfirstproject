604ab61ffc55397adbb95886abdc209409d8e390
letsmovetocloud <88219893+letsmovetocloud@users.noreply.github.com>
2022-03-10 17:24:44 +0530

    I have changed reagion&availability zone details
    I have changed reagion&availability zone details

--- ec2.tf
+++ ec2.tf
@@ -11,7 +11,7 @@ terraform {
 # Provider Block -line
 provider "aws" {
   profile = "default" 
-  region  = "us-east-1"
+  region  = "ap-south-1"
 }
 
 #Resource Block-developed by second develeoper 
@@ -30,7 +30,7 @@ module "vpc" {
   # VPC Basic Details
   name = "vpc-dev"
   cidr = "10.0.0.0/16"   
-  azs                 = ["us-east-1a"]
+  azs                 = ["ap-south-1a"]
   private_subnets     = ["10.0.1.0/24"]
   public_subnets      = ["10.0.101.0/24"]
 
